74b2096989bf8569805ffd00a486a2c0018ee160
Absher K <absheroamac@gmail.com>
2024-10-09 12:47:48 +0530

    Completed Project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+
+import { useState } from "react";
+import styles from "./App.module.css"
 
 function App() {
+
+  const [fullname, setFullname] = useState("")
+
+  const handleSubmit = (event)=>
+  {
+    event.preventDefault();
+    setFullname(event.target.firstName.value+" "+event.target.lastName.value);
+
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Full Name Display</h1>
+      <form className={styles.form} onSubmit={handleSubmit}>
+        <label >First Name:</label>
+        <input id="firstName" type="text" name="firstName"required></input>
+        <br/>
+        <label >Last Name:</label>
+        <input id="lastName" type="text" name="lastName"required></input>
+        <br/>
+        <button>Submit</button>
+      </form>
+
+      {fullname!=""&&<div>Full Name: {fullname}</div>}
+
+      
+      
     </div>
   );
 }
